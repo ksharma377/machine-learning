bb2de68eb126d27c996415e5a2db22b32e1fb654
Kshitij Sharma <ksharma377@gmail.com>
2020-07-13 02:09:43 +0530

    Added main file

--- univariate-linear-regression/src/generate_training_data.py
+++ univariate-linear-regression/src/generate_training_data.py
@@ -1,28 +1,34 @@
-# This file generates the training data for univariate linear regression.
+"""
+This file generates the training data for univariate linear regression.
 
-# Generates: "../data/training_data.csv"
-# Each row in the file represents [x, y]
+Generates: "training_data.csv"
+Location: "../data/"
+Each row in the file represents [x, y]
 
-# Input: x
-# Parameters: w0, w1
-# Output: y = w0 + (w1 * x)
-# Number of training examples: m
+Input: x
+Parameters: w0, w1
+Output: y = w0 + (w1 * x)
+Number of training examples: m
+"""
 
 import random
 
 w0 = 34.715
 w1 = -79.074
-m = 10000
 lower_limit = -100000
 upper_limit = 100000
 
+# Number of training examples
+m = 10000
+
+# Output filename
 filename = "../data/training_data.csv"
 
 print("\n*** Beginning training data generation ***\n")
 
 with open(filename, "w") as fp:
 	fp.write("x,y\n")
-	for i in range(m):
+	for _ in range(m):
 		x = random.uniform(lower_limit, upper_limit)
 		y = w0 + (w1 * x)
 		fp.write("{},{}\n".format(x, y))
---
+++ univariate-linear-regression/src/main.py
@@ -0,0 +1,36 @@
+"""
+This is the main file which builds the univariate linear
+regression model.
+
+Training data filename: "training_data.csv"
+Training data path: "../data/"
+
+The model trains on the above data and outputs the parameters
+and the accuracy. The number of training cycles is controlled
+by the variable "epochs".
+
+Input: x
+Parameters: w0, w1
+Output: y
+Heuristic: h(x) = w0 + (w1 * x)
+Number of training examples: m
+Batch size: b
+Learning rate: r
+Cost function = MSSE (Mean sum of squares of errors)
+	C(w0, w1) = (1 / 2m) sigma((h(x) - y)^2)
+Optimizer algorithm: Batch Gradient Descent
+"""
+
+import random
+
+"""
+Initializes the parameters
+"""
+def initialize_parameters():
+	global w0, w1, r, epochs
+	w0 = random.random()
+	w1 = random.random()
+	r = 0.1
+	epochs = 100
+
+initialize_parameters()
