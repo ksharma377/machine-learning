b62c4dd3d616a1d78f16a28e8e4b268c4183b9ba
Kshitij Sharma <ksharma377@gmail.com>
2020-07-13 01:02:10 +0530

    Added training data generator

---
+++ univariate-linear-regression/src/generate_training_data.py
@@ -0,0 +1,30 @@
+# This file generates the training data for univariate linear regression.
+
+# Generates: "../data/training_data.csv"
+# Each row in the file represents [x, y]
+
+# Input: x
+# Parameters:
+# 	w0 = 34.715 
+# 	w1 = 79.074
+# Output: y = w0 + w1 * x
+# Number of training examples: m
+
+import random
+
+w0 = 34.715
+w1 = -79.074
+m = 10000
+filename = "../data/training_data.csv"
+lower_limit = -100000
+upper_limit = 100000
+
+print("\n*** Beginning training data generation ***\n")
+
+with open(filename, "w") as fp:
+	for i in range(m):
+		x = random.uniform(lower_limit, upper_limit)
+		y = w0 + w1 * x
+		fp.write("{},{}\n".format(x, y))
+
+print("*** Trainig data generated successfully ***\n")
