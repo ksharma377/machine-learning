2b7f7a78431bc8084fc5a51eed21392c76d00ec4
Kshitij Sharma <ksharma377@gmail.com>
2020-07-18 00:31:47 +0530

    Added train and test split

--- univariate-linear-regression/src/main.py
+++ univariate-linear-regression/src/main.py
@@ -21,6 +21,9 @@ Optimizer algorithm: Batch Gradient Descent
 """
 
 import random
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 """
 Initializes the parameters
@@ -32,4 +35,38 @@ def initialize_parameters():
 	r = 0.1
 	epochs = 100
 
-initialize_parameters()
+"""
+Reads the data in csv format and splits into training and testing data (80-20)
+"""
+def read_data():
+	df = pd.read_csv('../data/training_data.csv')
+	global train_data, test_data
+	train_data, test_data = train_test_split(df, test_size = 0.2)
+
+def calculate_mse(batch):
+	m = len(batch)
+	error = 0
+	for row in batch.itertuples():
+		x, y = row.x, row.y
+		h = w0 + (w1 * x)
+		error += (h - y) ** 2
+	return error / (2 * m)
+
+"""
+Trains the linear regression model.
+1. Sample a batch of fixed size.
+2. Calculate the error for this batch.
+3. Update the parameters using Gradient Descent.
+4. Repeat for epochs.
+"""
+def train_model():
+	batch_size = 1000
+	for epoch in range(epochs):
+		batch = train_data.sample(n = batch_size)
+		error = calculate_mse(batch)
+		print("Epoch: {}, Error: {}".format(epoch, error))
+
+if __name__ == "__main__":
+	read_data()
+	initialize_parameters()
+	train_model()
